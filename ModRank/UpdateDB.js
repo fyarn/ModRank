9f82d781f92500b590bcd51b96c0b195a13dc5ca
fyarn <1361631+fyarn@users.noreply.github.com>
2017-09-04 14:54:13 -0700

    adding updatedb class to deal with daily downloads

---
+++ ModRank/UpdateDB.js
@@ -0,0 +1,11 @@
+﻿var fs = require('fs');
+
+//get stored variables
+var prefs = JSON.parse(fs.readFileSync('../protected/prefs.json', 'utf-8'));
+var devKey = prefs.DevKey;
+var updateIntervalInMS = prefs.UpdateIntervalInMS;
+
+//if time to update (default 1 day)
+if (new Date() - fs.statSync('../protected/db.json').ctime > updateIntervalInMS) {
+
+}
