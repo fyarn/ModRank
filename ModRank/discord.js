a0b50c69f2f9d6dd7de4d06b0012bb9112f3782d
fyarn <1361631+fyarn@users.noreply.github.com>
2017-09-08 20:11:36 -0700

    - more debugging of discord bot

--- ModRank/discord.js
+++ ModRank/discord.js
@@ -21,7 +21,8 @@ function discord(app) {
                     return console.log('login fail: ' + err);
                 }
                 else {
-                    client.login(JSON.parse(body).token).catch(setTimeout(makeRequest, (1000 * 60)));
+                    console.log(body);
+                    client.login(JSON.parse(body).token).catch((e) => console.log(e));
                 }
             }
         );
