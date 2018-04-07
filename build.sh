16730bd056a96f36e91cc3e93647ed2fce53f61d
Nick Fisher <nick0fisher@gmail.com>
2018-04-07 15:34:25 -0700

    Refine dockerfiles to track github maser

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 echo "Deploying...";
 pushd /opt/ModRank;
-docker-compose down;
-docker pull nick0fisher/modrank;
-docker-compose up &;
+docker-compose stop node;
+git stash;
+git pull origin master;
+docker-compose up -d --build > /dev/null;
 popd;
-
