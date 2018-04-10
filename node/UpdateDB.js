c4b37265fa31678729505e677c614d1ad03b0900
Nick Fisher <nick0fisher@gmail.com>
2018-04-09 23:34:44 -0700

    Refactor UpdateDB to OO/ES6

--- node/UpdateDB.js
+++ node/UpdateDB.js
@@ -1,104 +1,121 @@
-﻿var fs = require('fs');
-var request = require('request');
-var parse = require('./Parser');
-var mongojs = require('mongojs');
+﻿import {
+  readFileSync
+} from "fs";
+import request from "request";
+import parse from "./Parser";
+import mongojs from "mongojs";
+import mongoist from "mongoist";
+import {log, warning, error} from "console";
+import { builtinModules } from "module";
 
-function DatabaseUpdater(app) {
-   DatabaseUpdater.prototype.Update = function (appid, forced = false) {
-      //get stored variables
-      var devKey;
-      try {
-         var contents = JSON.parse(fs.readFileSync('./Protected/keys.json'));
-         devKey = contents.DevKey;
-      } catch (error) {
-         devKey = process.env.DevKey
+class SteamWorker {
+
+  constructor(app, steamid) {
+    this.app = app;
+    this.steamid = steamid;
+    this.db = mongoist(mongojs(app.get('DBConnection')))[steamid];
+    this.updateIntervalInMS = 10800000 * 4;
+    this.results = [];
+    this.totalItemCount = -1;
+  }
+
+  async Update(forced = false) {
+    //get stored variables
+    var devKey = process.env.DevKey;
+    try {
+      devKey = JSON.parse(readFileSync('./Protected/keys.json')).DevKey;
+    } catch (err) {
+      error(err);
+    }
+    this.DevKey = devKey;
+
+    try {
+      lastUpdateRecord = await db.findOne({ id: "last_update" });
+    } catch (err) {
+      error(err);
+    }
+    this.lastUpdate = lastUpdateRecord && lastUpdateRecord.last_update;
+    log("Last Update: " + this.lastUpdate);
+
+    let forced = process.env.ForceUpdate !== undefined;
+    let debug = process.env.debug;
+    //if time to update (default 12 hours) or file doesn't exist
+    if (forced ||
+      lastUpdate == null ||
+      (!debug && new Date().getTime() - lastUpdate > updateIntervalInMS)) {
+      let options = {
+        url: 'https://api.steampowered.com/IPublishedFileService/QueryFiles/v1',
+        method: 'GET',
+        qs: {
+          'key': this.DevKey,
+          'query_type': 0,
+          'page': 0, // incremented to 1 on first call
+          'numperpage': '100',
+          'creator_appid': this.steamid,
+          'appid': this.steamid,
+          'requiredtags': '',
+          'excludedtags': '',
+          'required_flags': '',
+          'omitted_flags': '',
+          'search_text': '',
+          'filetype': 0,
+          'child_publishedfileid': 0,
+          'days': 7,
+          'include_recent_votes_only': false,
+          'required_kv_tags': {},
+          'return_vote_data': true,
+          'return_tags': true,
+          'return_kv_tags': true,
+          'return_previews': false,
+          'return_children': true,
+          'return_short_description': true,
+          'return_for_sale_data': false,
+          'return_playtime_stats': 7
+        }
+      };
+      requestUntillFilled(options);
+      // if cache isn't already populated, condition on startup
+    } else if (app.get('Cacher') === undefined) {
+      let docs = await db.find({ $id: { $type: "number" } });
+      parseRequests(docs, true);
+    }
+  }
+
+  parseRequests(docs, useDB = false) {
+    parse(docs, this.steamid, this.app, () => console.log('Downloaded, parsing...'), useDB);
+  }
+
+  requestUntillFilled(options) {
+    options.qs.page++;
+    request(options, (err, response, body) => {
+      if (err) {
+        error(err);
       }
-      // 10800000ms = refresh every 3 hours
-      var updateIntervalInMS = 10800000 * 4;
-      var results = [];
-      var totalItemCount = -1;
-      var collection = mongojs(app.get('DBConnection'))["Steam_App_" + appid];
-      collection.findOne({ id: "last_update" }, (err, item) => {
-         var lastUpdate = item && item.last_update;
-         console.log("Last Update: " + lastUpdate);
-         forced = process.env.ForceUpdate !== undefined;
-         var debug = process.env.debug;
-         //if time to update (default 12 hours) or file doesn't exist
-         if (forced || lastUpdate == null || (!debug && new Date().getTime() - lastUpdate > updateIntervalInMS)) {
-            var options = {
-               url: 'https://api.steampowered.com/IPublishedFileService/QueryFiles/v1',
-               method: 'GET',
-               qs: {
-                  'key': devKey,
-                  'query_type': 0,
-                  'page': '1',
-                  'numperpage': '100',
-                  'creator_appid': 294100,
-                  'appid': 294100,
-                  'requiredtags': '',
-                  'excludedtags': '',
-                  'required_flags': '',
-                  'omitted_flags': '',
-                  'search_text': '',
-                  'filetype': 0,
-                  'child_publishedfileid': 0,
-                  'days': 7,
-                  'include_recent_votes_only': false,
-                  'required_kv_tags': {},
-                  'return_vote_data': true,
-                  'return_tags': true,
-                  'return_kv_tags': true,
-                  'return_previews': false,
-                  'return_children': true,
-                  'return_short_description': true,
-                  'return_for_sale_data': false,
-                  'return_playtime_stats': 7
-               }
-            };
-            requestUntillFilled();
-            // if cache isn't already populated, condition on startup
-         } else if (app.get('Cacher') === undefined) {
-            collection.find({$id : {$type: "number"}}, (err, docs) => {
-               parse(docs, appid, app, () => console.log('done'), /* useDatabase */ true);
-            });
-         }
 
-         function rufCallback() {
-            console.log("Finished with " + results.length + " results");
-            parse(results, appid, app, () => console.log('done'));
-         }
+      body = JSON.parse(body);
+      if (body.response.publishedfiledetails === undefined) {
+        log("Finished with " + this.results.length + " results");
+        return parseRequests(this.results);
+      }
 
-         function requestUntillFilled(page = 1) {
-            options.qs.page = page;
-            request(options, function (error, response, body) {
-               if (!error) {
-                  body = JSON.parse(body);
-                  if (body.response.publishedfiledetails !== undefined) {
-                     //if first call
-                     if (totalItemCount < 0) {
-                        totalItemCount = body.response.total;
-                        console.log("Total Items: " + totalItemCount);
-                     }
+      //if first call
+      if (this.totalItemCount < 0) {
+        this.totalItemCount = body.response.total;
+        log("Total Items: " + this.totalItemCount);
+      }
 
-                     results = results.concat(body.response.publishedfiledetails);
-                     var len = body.response.publishedfiledetails.length;
-                     console.log("Result Count: " + results.length);
+      this.results = this.results.concat(body.response.publishedfiledetails);
+      log("Result Count: " + this.results.length);
 
-                     //if done
-                     if (results.length >= totalItemCount) {
-                        return rufCallback();
-                     }
-                     else {
-                        requestUntillFilled(page + 1);
-                     }
-                  }
-                  else {
-                     return rufCallback();
-                  }
-               }
-            });
-         }
-      });
-   };
+      //if done
+      if (results.length >= totalItemCount) {
+        log("Finished with " + this.results.length + " results");
+        return parseRequests(this.results);
+      }
+
+      requestUntillFilled(options);
+    });
+  }
 }
-module.exports = DatabaseUpdater;
+
+export default SteamWorker;
