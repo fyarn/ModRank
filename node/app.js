cf6c1d4be8396b38bb9d56f6f31345225a48f3cc
Nick Fisher <nick0fisher@gmail.com>
2018-04-10 18:06:21 -0700

    Refactor App.js to follow OO design

--- node/app.js
+++ node/app.js
@@ -12,102 +12,70 @@ var item = require('./routes/item');
 var compare = require('./routes/compare');
 var chart = require('./routes/chart');
 
-var router = express.Router();
 var visitor = ua('UA-64719618-2');
 var mongojs = require('mongojs');
 var SteamAPIWorker = require('./SteamAPIWorker');
 
-function SetupApp() {
+class App {
+  constructor() {
     // view engine setup
-    app = express();
-    //app.set('SUPPORTED_APPS', SUPPORTED_APPS);
-    app.set('visitor', visitor);
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
+    let app = express()
+      .set('visitor', visitor)
+      .set('views', path.join(__dirname, 'views'))
+      .set('view engine', 'jade')
+      .set('DBConnection', 'mongodb://mongodb:27017')
+      .set('parserRegex', /(https:\/\/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=)?(\d+)|([Rr][Aa][Nn][Dd]([Oo][Mm])?)/)
+      .set('steamRegex', /https:\/\/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=/)
+      .set('idRegex', /\d+/);
 
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, '/public')));
-
-    app.set('DBConnection', 'mongodb://mongodb:27017');
-
-    app.use('/', index);
-    app.use('/item', item);
-    app.use('/compare', compare);
-    app.use('/chart', chart);
-    app.use('/api', router);
-
-    //replace double slashes after URL
-    app.use('//', function (req, res) {
-        res.redirect(req.url.replace(/([^:]\/)\/+/g, "$1"));
-    });
-
-    router.get('/', function (req, res) {
-        res.json({
-            message: 'Connection succesful: ModRank API v1.0.0'
-        });
+    let router = express.Router()
+      .get('/', (req, res) => {
+        res.json({ message: 'Connection succesful: ModRank API v1.0.0' });
         visitor.event("API", "Connection Test").send();
-    });
-
-    app.get('/robots.txt', function (req, res) {
-        res.send("User-agent: *\nDisallow:\/*random*\nDisallow:\/*rand*\n");
-    });
-
-
-    router.route('items', function (req, res) {
-        res.json({
-            message: 'Connection succesful: ModRank Items API v1.0.0'
-        });
+      })
+      .get('/robots.txt', (req, res) =>
+        res.send("User-agent: *\nDisallow:\/*random*\nDisallow:\/*rand*\n")
+      )
+      .route('/items', (req, res) => {
+        res.json({ message: 'Connection succesful: ModRank Items API v1.0.0' });
         visitor.event("API", "Item Connection Test").send();
-    });
-
-    router.route('/items/:item_id').get(async function (req, res) {
-        let cache = app.get('Cache');
-        let item = await cache.getItem(sanitizer.value(req.params.item_id.substring(1), app.get('parserRegex')));
+      })
+      .route('/items/:item_id').get(async (req, res) => {
+        let item = await app.get('Cache').getItem(
+          sanitizer.value(req.params.item_id, app.get('parserRegex'))
+        );
         visitor.event("API", "Item Lookup", req.params.item_id).send();
         //item not found
-        item === null ?
-          res.status(404).send({ error: "404 item not found" }) :
-          res.json(doc);
-    });
+        item !== null ?
+          res.json(doc) :
+          res.status(404).send({ error: "404 item not found" });
+      });
 
-    // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });
-
-    // error handler
-    app.use(function (err, req, res, next) {
+    app.use(logger('dev'))
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({ extended: false }))
+      .use(cookieParser())
+      .use(express.static(path.join(__dirname, '/public')))
+      .use('/', index)
+      .use('/item', item)
+      .use('/compare', compare)
+      .use('/chart', chart)
+      .use('/api', this.router)
+      //replace double slashes after URL
+      .use('//', (req, res) => res.redirect(req.url.replace(/([^:]\/)\/+/g, "$1")))
+      // catch 404 and forward to error handler
+      .use((req, res, next) => next(new Error('Not Found').status(404)))
+      // error handler
+      .use((err, req, res, next) => {
         // set locals, only providing error in development
         res.locals.message = err.message;
         res.locals.error = req.app.get('env') === 'development' ? err : {};
+        res.status(err.status || 500).render('error');
+      });
 
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
-    });
-
-    let DBUpdater = new SteamAPIWorker(app, '294100');
-    /*for (appid in ['294100']) {
-       console.log(appid);
-       //DBUpdater.Update(appid, true);
-    }*/
-    DBUpdater.keepUpToDate();
-
-    // TODO: move the setinterval into DBUpdater.Update
-    // check for update hourly, though only update if have waited > 12 hours
-    
-
-    app.set('parserRegex', /(https:\/\/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=)?(\d+)|([Rr][Aa][Nn][Dd]([Oo][Mm])?)/);
-    app.set('steamRegex', /https:\/\/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=/);
-    app.set('idRegex', /\d+/);
-    module.exports = app;
+    new SteamAPIWorker(app, '294100').keepUpToDate();
+    return app;
+  }
 }
 
-SetupApp();
+module.exports = new App();
